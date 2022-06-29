88ffb1724acc638ad3ddad0928ce03a7d858e616
Karthinivash <sr.karthinivash@Karthinivashs-MacBook-Pro.local>
2022-06-29 10:46:52 +0530

    [Karthinivsh: Refactored]

--- src/main/java/Factorial.java
+++ src/main/java/Factorial.java
@@ -8,7 +8,9 @@ public class Factorial {
     }
 
     public int factorial() {
-        if (number == 0) return 1;
+        if (number == 0) {
+            return 1;
+        }
         for (int i = 1; i <= number ; i++) {
             fact = fact * i;
         }
